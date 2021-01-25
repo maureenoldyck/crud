86b361df627dc23e5ca0a4a5141e82162817434e
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-25 12:34:03 +0100

    Add base files

---
+++ classes/DatabaseManager.php
@@ -0,0 +1,25 @@
+<?php
+
+// This class will manage the connection to the database
+// It will be passed on the other classes who need it
+class DatabaseManager
+{
+    // These are private: only this class needs them
+    private $host;
+    private $name;
+    private $password;
+    // This one is public, so we can use it outside of this class
+    // We could also use a private variable and a getter (but let's not make things too complicated at this point)
+    public $database;
+
+    public function __construct(string $host, string $name, string $password)
+    {
+        // TODO: Set any user and password information
+    }
+
+    public function connect()
+    {
+        // TODO: make the connection to the database
+        $this->database = null;
+    }
+}
