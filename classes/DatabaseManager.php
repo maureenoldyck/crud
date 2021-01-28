2293f26773f03e78067813315acb0e3f8a388d73
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-28 09:49:06 +0100

    Add line to connection to disable emulated prepared statements and to only use real prepared statements

--- classes/DatabaseManager.php
+++ classes/DatabaseManager.php
@@ -28,6 +28,7 @@ class DatabaseManager
         try {
             $this->database = new PDO("mysql:host=$this->host;dbname=$this->database", $this->name, $this->password);
             // set the PDO error mode to exception
+            $this->database->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
